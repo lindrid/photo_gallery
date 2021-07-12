443383849d2f2c17710cf964630cb29b757f9e42
lindrid <lindrid32@gmail.com>
2021-07-12 17:12:19 -0400

    gson serialization instead ordinary string

--- app/src/main/java/com/example/photogallery/Flickr.kt
+++ app/src/main/java/com/example/photogallery/Flickr.kt
@@ -4,10 +4,13 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.photogallery.api.FlickrApi
+import com.example.photogallery.api.FlickrResponse
+import com.example.photogallery.api.PhotoResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 private const val TAG = "Flickr"
@@ -19,27 +22,33 @@ class Flickr {
     val retrofit: Retrofit = Retrofit.Builder()
       .baseUrl("https://www.flickr.com/")
       // Factory is for convert okhttp3.ResponseBody to String (See FlickrApi)
-      .addConverterFactory(ScalarsConverterFactory.create())
+      .addConverterFactory(GsonConverterFactory.create())
       .build()
 
     api = retrofit.create(FlickrApi::class.java)
   }
 
-  fun fetchContents(): LiveData<String> {
-    val responseLiveData: MutableLiveData<String> = MutableLiveData()
-    val flickrRequest: Call<String> = api.fetchContents()
+  fun fetchPhotos(): LiveData<List<GalleryItem>> {
+    val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
+    val flickrRequest: Call<FlickrResponse> = api.fetchPhotos()
 
-    flickrRequest.enqueue(object : Callback<String>
-    {
-      override fun onFailure(call: Call<String>, t: Throwable) {
+    flickrRequest.enqueue(object : Callback<FlickrResponse> {
+      override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
         Log.e (TAG, "Failed to fetch photos", t)
       }
 
-      override fun onResponse(call: Call<String>, response: Response<String>) {
-        val s: String = response.body().toString()
-        Log.d (TAG, "Response received: ${s.length}")
-        responseLiveData.value = s
+      override fun onResponse(call: Call<FlickrResponse>, response: Response<FlickrResponse>) {
+        Log.d (TAG, "Response received")
+
+        val flickrResponse: FlickrResponse? = response.body()
+        val photoResponse: PhotoResponse? = flickrResponse?.photos
+        var galleryItems: List<GalleryItem> = photoResponse?.galleryItems?: mutableListOf()
+
+        galleryItems = galleryItems.filterNot { it.url.isBlank() }
+        responseLiveData.value = galleryItems
       }
     })
+
+    return responseLiveData
   }
 }
