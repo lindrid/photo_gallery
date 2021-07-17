ff1f2266c18cc6a3699c675eb7feef1c252d2b39
lindrid <lindrid32@gmail.com>
2021-07-17 01:05:00 -0400

    585

--- app/src/main/java/com/example/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/example/photogallery/ThumbnailDownloader.kt
@@ -1,6 +1,7 @@
 package com.example.photogallery
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Message
@@ -14,11 +15,42 @@ import java.util.concurrent.ConcurrentHashMap
 private const val TAG = "ThumbnailDownloader"
 private const val MESSAGE_DOWNLOAD = 0
 
-class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
+// responseHandler это Handler из главного потока, связанный с Looper из главного потока
+// второй параметр - слушатель для передачи ответов (загруженных изображений)
+// запрашивающей стороне (главному потоку)
+class ThumbnailDownloader<in T> (
+    private val responseHandler: Handler,
+    private val onThumbnailDownloaded: (T, Bitmap) -> Unit
+  ) : HandlerThread(TAG), LifecycleObserver
+{
   private var hasQuit = false
   private lateinit var requestHandler: Handler
   private val requestMap = ConcurrentHashMap<T, String>()
   private val flickr = Flickr(ApiSingleton.get().flickr)
+
+  val fragmentLifecycleObserver: LifecycleObserver = object : LifecycleObserver {
+    @OnLifecycleEvent (Lifecycle.Event.ON_CREATE)
+    fun setup() {
+      Log.i (TAG,"Starting background thread")
+      start()
+      looper
+    }
+
+    @OnLifecycleEvent (Lifecycle.Event.ON_DESTROY)
+    fun tearDown() {
+      Log.i (TAG, "Destroying background thread")
+      quit()
+    }
+  }
+
+  val viewLifecycleObserver: LifecycleObserver = object : LifecycleObserver {
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun clearQueue() {
+      Log.i (TAG, "Clearing all requests from queue")
+      requestHandler.removeMessages(MESSAGE_DOWNLOAD)
+      requestMap.clear()
+    }
+  }
 
   override fun quit(): Boolean {
     hasQuit = true
@@ -28,6 +60,7 @@ class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
   fun queueThumbnail (target: T, url: String) {
     Log.i (TAG, "Got a url: $url")
     requestMap[target] = url
+    requestHandler = Handler()
     requestHandler.obtainMessage(MESSAGE_DOWNLOAD, target).sendToTarget()
   }
 
@@ -56,15 +89,13 @@ class ThumbnailDownloader<in T> : HandlerThread(TAG), LifecycleObserver {
   private fun handleRequest(target: T) {
     val url = requestMap[target] ?: return
     val bitmap = flickr.fetchPhoto(url) ?: return
-  }
 
-  @OnLifecycleEvent (Lifecycle.Event.ON_CREATE)
-  fun setup() {
-    Log.i (TAG,"Starting background thread")
-  }
-
-  @OnLifecycleEvent (Lifecycle.Event.ON_DESTROY)
-  fun tearDown() {
-    Log.i (TAG, "Destroying background thread")
+    responseHandler.post(Runnable {
+      if (requestMap[target] != url || hasQuit) {
+        return@Runnable
+      }
+      requestMap.remove(target)
+      onThumbnailDownloaded(target, bitmap)
+    })
   }
 }
